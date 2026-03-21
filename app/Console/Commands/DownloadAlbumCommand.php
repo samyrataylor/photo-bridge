7ae46f4f4d49c081ace2a055a4cc98a1d407a282
Samyra Taylor <samyra.taylor@envorra.com>
2026-03-21 19:29:06 -0400

    chore: add download command

---
+++ app/Console/Commands/DownloadAlbumCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\CloudUser;
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class DownloadAlbumCommand extends Command
+{
+    protected $signature = 'album:download {user} {album} {--command-only}';
+
+    public function handle(): void
+    {
+        $io = new SymfonyStyle($this->input, $this->output);
+        $user = CloudUser::where('short_name', $this->argument('user'))->first();
+
+        if (! $user) {
+            $io->error('User does not exist');
+
+            return;
+        }
+
+        $album = $user->albums()->where('name', $this->argument('album'))->first();
+
+        if (! $album) {
+            $io->error('Album does not exist');
+
+            return;
+        }
+
+        $command = $user->iCloudPD()->builder()->directory($album->downloadPath())->album($album->name)->build();
+
+        if ($this->option('command-only')) {
+            $io->writeln($command);
+
+            return;
+        }
+
+        $io->error('Not set up to run command');
+    }
+}
