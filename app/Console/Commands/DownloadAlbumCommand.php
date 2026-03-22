35dfed524a0c33220107f1b702f03f0a0a69397f
Samyra Taylor <samyra.taylor@envorra.com>
2026-03-22 14:47:23 -0400

    chore: update download commnad

--- app/Console/Commands/DownloadAlbumCommand.php
+++ app/Console/Commands/DownloadAlbumCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\iCloudPD\Options\Size;
 use App\Models\CloudUser;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -29,7 +30,15 @@ class DownloadAlbumCommand extends Command
             return;
         }
 
-        $command = $user->iCloudPD()->builder()->directory($album->downloadPath())->album($album->name)->build();
+        $command = $user->iCloudPD()
+            ->builder()
+            ->xmpSidecar()
+            ->size(Size::Original)
+            ->size(Size::Adjusted)
+            ->size(Size::Alternative)
+            ->directory($album->downloadPath())
+            ->album($album->name)
+            ->build();
 
         if ($this->option('command-only')) {
             $io->writeln($command);
